1547f973e18202b1e9a3254fc45900024c27f5d7
Joel Grayson <joel@joelgrayson.com>
2025-06-28 11:21:13 -0700

    design

--- Data/upload-to-firestore.js
+++ Data/upload-to-firestore.js
@@ -1,7 +1,7 @@
 const fs=require('fs').promises;
 
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
 const serviceAccount=require('../Private/joelgrayson-techmap.json');
 
