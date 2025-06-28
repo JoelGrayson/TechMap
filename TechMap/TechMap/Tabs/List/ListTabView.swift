52de7e548169f0479507aedb76e81c7cb6ad9a4e
Joel Grayson <joel@joelgrayson.com>
2025-06-28 15:45:55 -0700

    ListTabView UI looks better and allows user to click on a company to see more details

--- TechMap/TechMap/Tabs/List/ListTabView.swift
+++ TechMap/TechMap/Tabs/List/ListTabView.swift
@@ -19,34 +19,49 @@ struct ListTabView: View {
         }
 
     var body: some View {
-        Text("Visited \(checks.count) of \(companies.count) companies")
-        
-        Text("Visited")
-            .sectionTitle()
-        
-        List(checks) { check in
-            let associatedCompany = companies.first(where: { $0.id == check.companyId })
-            if let associatedCompany {
-                HStack {
-                    InlineLogo(imageName: associatedCompany.imageName)
-                    Text(associatedCompany.name)
-                    Spacer()
-                    Text(RelativeDateFormatter.format(check.createdAt))
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Text("Visited \(checks.count) of \(companies.count) companies")
+                
+                Text("Visited")
+                    .sectionTitle()
+                
+                List(checks) { check in
+                    let associatedCompany = companies.first(where: { $0.id == check.companyId })
+                    
+                    NavigationLink(value: associatedCompany) {
+                        HStack {
+                            if let associatedCompany {
+                                InlineLogo(imageName: associatedCompany.imageName)
+                                Text(associatedCompany.name)
+                                Spacer()
+                                Text(RelativeDateFormatter.format(check.createdAt))
+                                    .padding(.vertical, 4)
+                            } else {
+                                // there is a bug
+                                Text(check.companyId)
+                            }
+                        }
+                    }
                 }
-            } else {
-                // there is a bug
-                Text(check.companyId)
+                .navigationDestination(for: Company.self) { company in
+                    Text(company.name)
+                    // CompanyDetails(company: company, height: .infinity, checked: <#T##Bool#>, markAsVisited: <#T##() -> Void#>, uncheck: <#T##() -> Void#>)
+                }
+                .listStyle(.plain)
+                
+                Text("Not Visited Yet")
+                    .sectionTitle()
+                
+                List(notVisitedYet) { company in
+                    HStack {
+                        InlineLogo(imageName: company.imageName)
+                        Text(company.name)
+                    }
+                }
+                .listStyle(.plain)
             }
-        }
-        
-        Text("Not Visited Yet")
-            .sectionTitle()
-        
-        List(notVisitedYet) { company in
-            HStack {
-                InlineLogo(imageName: company.imageName)
-                Text(company.name)
-            }
+            .padding()
         }
     }
 }
