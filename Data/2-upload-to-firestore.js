c87a99159c12237e97fc757f28b280d3b3cf3e4a
Joel Grayson <joel@joelgrayson.com>
2025-07-05 09:40:55 -0700

    renamed from output to finalOutput so that searching with final-output.json works

--- Data/2-upload-to-firestore.js
+++ Data/2-upload-to-firestore.js
@@ -40,29 +40,29 @@ async function main() {
     // Some of these files were edited so please if you (me) rebuild them, do it with caution.
 
     console.log('Uploading Bay Area 1');
-    await uploadJSON('./2-geocoded/bay-area/1/output-with-wiki.json', 'bay-area');
+    await uploadJSON('./2-geocoded/bay-area/1/final-output.json', 'bay-area');
     console.log('\nUploading Bay Area 2');
-    await uploadJSON('./2-geocoded/bay-area/2/output-with-wiki.json', 'bay-area');
+    await uploadJSON('./2-geocoded/bay-area/2/final-output.json', 'bay-area');
     console.log('\nUploading Bay Area 3');
-    await uploadJSON('./2-geocoded/bay-area/3/output.json', 'bay-area');
+    await uploadJSON('./2-geocoded/bay-area/3/final-output.json', 'bay-area');
 
     console.log('\nUploading NYC 1');
-    await uploadJSON('./2-geocoded/nyc/1/output-with-wiki.json', 'nyc');
+    await uploadJSON('./2-geocoded/nyc/1/final-output.json', 'nyc');
     console.log('\nUploading NYC 2');
-    await uploadJSON('./2-geocoded/nyc/2/output.json', 'nyc');
+    await uploadJSON('./2-geocoded/nyc/2/final-output.json', 'nyc');
 
     console.log('\nUploading Seattle');
-    await uploadJSON('./2-geocoded/seattle/1/output.json', 'seattle');
+    await uploadJSON('./2-geocoded/seattle/1/final-output.json', 'seattle');
 
 
     // Create one large file called all-companies.json. It is useful since I can paste it into LLMs to tell me if there are any duplicates or errors.
     const forAllCombined=[
-        './2-geocoded/bay-area/1/output-with-wiki.json',
-        './2-geocoded/bay-area/2/output-with-wiki.json',
-        './2-geocoded/bay-area/3/output.json',
-        './2-geocoded/nyc/1/output-with-wiki.json',
-        './2-geocoded/nyc/2/output.json',
-        './2-geocoded/seattle/1/output.json'
+        './2-geocoded/bay-area/1/final-output.json',
+        './2-geocoded/bay-area/2/final-output.json',
+        './2-geocoded/bay-area/3/final-output.json',
+        './2-geocoded/nyc/1/final-output.json',
+        './2-geocoded/nyc/2/final-output.json',
+        './2-geocoded/seattle/1/final-output.json'
     ];
     let allCompanies=[];
     for (const fileName of forAllCombined) {
